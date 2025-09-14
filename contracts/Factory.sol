8912ff021190f43184890dbb3929a50224f8a4f2
Majid Aziev <majidaziev007@yandex.ru>
2025-09-14 21:53:36 +0300

    Preparing tests for contracts

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
+ // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Factory {}
+contract Factory {
+    address public owner;
+    uint256 public immutable fee;
+
+    constructor(uint256 _fee){
+        owner = msg.sender;
+        fee = _fee;
+    }
+}
--- test/Factory.js
+++ test/Factory.js
@@ -1,7 +1,34 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { Fuzzy_Bubbles } = require("next/font/google");
 
 describe("Factory", function () {
+  const FEE = ethers.parseUnits("0.001", 18);
 
-})
+  async function deployFactoryFixture() {
+    // Fetch accout
+    const [deployer] = await ethers.getSigners();
+    // Fetch the contract
+    const Factory = await ethers.getContractFactory("Factory");
+    // Deploy the contract
+
+    const factory = await Factory.deploy(FEE);
+
+    return { factory, deployer };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the fee", async function () {
+      const { factory } = await loadFixture(deployFactoryFixture);
+      expect(await factory.fee()).to.equal(FEE);
+    });
+
+    it("Should set the owner", async function () {
+      const { factory, deployer } = await loadFixture(deployFactoryFixture);
+      expect(await factory.owner()).to.equal(deployer.address);
+    });
+  });
+});
