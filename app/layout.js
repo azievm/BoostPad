6f6d688f52098cbc6ed3f97200ea23c2d3fb183b
Majid Aziev <majidaziev007@yandex.ru>
2025-09-17 02:47:40 +0300

    Frontend
    Started writing fronted in React Js. Created the basics

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,17 @@
 import { Nabla } from "next/font/google";
 import "./globals.css";
 
-const nabla = Nabla({ subsets: ['latin'] })
+const nabla = Nabla({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "fun.pump",
+  title: "boost.pad",
   description: "create token listings",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${nabla.className}`}>
-        {children}
-      </body>
+      <body className={`${nabla.className}`}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,26 +1,35 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Header from "./components/Header"
-import List from "./components/List"
-import Token from "./components/Token"
-import Trade from "./components/Trade"
+import Header from "./components/Header";
+import List from "./components/List";
+import Token from "./components/Token";
+import Trade from "./components/Trade";
 
 // ABIs & Config
-import Factory from "./abis/Factory.json"
-import config from "./config.json"
-import images from "./images.json"
+import Factory from "./abis/Factory.json";
+import config from "./config.json";
+import images from "./images.json";
 
 export default function Home() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  async function loadBlockchainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div className="page">
-
-      <h1 style={{ padding: "1em" }}>fun.pump</h1>
-
+      <Header account={account} setAccount={setAccount} />
     </div>
   );
 }
